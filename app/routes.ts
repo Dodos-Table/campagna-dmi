edca0bfa1edc49dfa9db7dc8014fc6959e9969a5
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2026-08-05 12:40:00 +0000

    Update routes.ts
    nascondendo le skill

--- app/routes.ts
+++ app/routes.ts
@@ -3,9 +3,9 @@ import { type RouteConfig, index, layout, prefix, route } from "@react-router/de
 export default [
     layout("routes/__layouts/main-layout.tsx", [
         index("routes/index.tsx"),
-        route("skill", "routes/__layouts/skill-layout.tsx", [
+        /*route("skill", "routes/__layouts/skill-layout.tsx", [
             index("routes/skill/index.tsx"),
-            route(":creatura", "routes/skill/creatura.tsx"),
+            route(":creatura", "routes/skill/creatura.tsx"),*/
         ]),
         route("npc", "routes/__layouts/simple-layout.tsx", [
             index("routes/npc/index.tsx")
